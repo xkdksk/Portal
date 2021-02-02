64dcc27c0e0b2058bba25dc44054abb33dd77a37
ulgerang <ulgerang@gmail.com>
2021-02-02 10:21:08 +0900

    트리거 when enable

---
+++ Assets/EventTriggerWhenEnable.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventTriggerWhenEnable : MonoBehaviour
+{
+    public Animator anim;
+
+    public string triggerName;
+
+    private void OnEnable()
+    {
+
+        anim.SetTrigger(triggerName);
+    }
+}
